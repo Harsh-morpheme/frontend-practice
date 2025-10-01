f95dd54d68ebc7fc2e5b18199ae473a3aa149cb7
Harsh-morpheme <harshsharma@webnexus.in>
2025-10-01 11:27:02 +0530

    css updated

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -12,7 +12,7 @@ function TaskForm() {
         };
     
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className=" flex items-center justify-center bg-gray-100">
   <div className="w-full max-w-full p-6 bg-white rounded-xl shadow-lg">
     <h1 className="text-2xl font-bold text-center mb-6">Task Manager</h1>
     <form className="flex gap-3" onSubmit={handleInput}>
