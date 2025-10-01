e054d8f98962c9a31e0162ad646f13a38b1839e9
Harsh-morpheme <harshsharma@webnexus.in>
2025-10-01 11:26:12 +0530

    tasklist component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <TaskForm/>
+      <TaskList/>
     </>
   )
 }
